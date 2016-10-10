ca81d1650fda6e10e5e2f3dfb3239a739c82338a
zhangxiaang <15019277742@163.com>
2016-10-10 17:03:48 +0800

    process2在process1等待期间修改共享内存

--- shm/shm_normal.cpp
+++ shm/shm_normal.cpp
@@ -19,10 +19,15 @@ int main(int argc, char *argv[]) {
     fd = open(argv[1], O_CREAT | O_RDWR, 00777);
     p_map = (people *) mmap(NULL, sizeof(people) * 10, PROT_READ | PROT_WRITE,
                             MAP_SHARED, fd, 0);
-
+//    int tempAge = 99;
+    char name[] = {'A', 'a', 'n', 'o'};
     for (i = 0; i < 10; i++) {
         printf("name: %s age %d;\n", (*(p_map + i)).name, (*(p_map + i)).age);
+        if (i == 9) {
+            memcpy((*(p_map + i)).name, &name, 4);
+        }
     }
+
     munmap(p_map, sizeof(people) * 10);
     return 1;
 }
--- shm/shmdemo.cpp
+++ shm/shmdemo.cpp
@@ -22,21 +22,33 @@ int main(int argc, char *argv[]) {
     people *p_map;
     char temp;
 
-    fd = open(argv[1], O_CREAT | O_RDWR | O_TRUNC, 00777);
+    fd = open(argv[1], O_CREAT | O_RDWR | O_TRUNC, 00777);//打开参数列表指定的file
     lseek(fd, sizeof(people) * 5 - 1, SEEK_SET);
-    write(fd, "", 1);
+    printf("people的size为:%d \n", (int) sizeof(people));
+    write(fd, "Aaron", 1);
 
-    p_map = (people *) mmap(NULL, sizeof(people) * 10, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);//这一步干了啥
+    p_map = (people *) mmap(NULL, sizeof(people) * 10, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    printf("系统自动给p_map选择的addr为:%p \n", &p_map);//页内存地址
+
+    //mmap()第一个参数为start的位置,代表讓系統自動選定地址，映射成功後返回該地址
     close(fd);
 
     temp = 'A';
     for (int j = 0; j < 10; ++j) {
         temp += 1;
-        memcpy((*(p_map + j)).name, &temp, 2);//memcpy是做甚的
+        memcpy((*(p_map + j)).name, &temp, 2);//把temp地址开头的连续几个字节写入到(*(p_map + j)).name里面
         (*(p_map + j)).age = 20 + j;
+
+        printf("p_map->%d:addr为:%p \n", j, &*(p_map + j));
     }
     printf("初始化完成\n");
     sleep(10);//seconds
+
+    //再次输出内容  看看在sleep期间内容是否被修改了
+
+    for (int i = 0; i < 10; i++) {
+        printf("name: %s age %d;\n", (*(p_map + i)).name, (*(p_map + i)).age);
+    }//果然 在sleep期间通过process2修改了p_map指向的共享内存区域内容也相应的修改了i == 9 的情况
     munmap(p_map, sizeof(p_map) * 10);//释放和共享内存的连接
     printf("释放和共享内存的连接\n");
 
